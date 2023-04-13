2f590ae284dfa305847ff9c8b3018466a452623c
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-04-13 18:49:30 +0530

    The server has been set up with dummy data

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,23 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
+const { chats } = require("./data/data");
 
 const app = express();
+dotenv.config();
 
-app.listen(5000, console.log("Server started on PORT 5000"));
+app.get("/", (req, res) => {
+  res.send("Api is running successfully");
+});
+
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  const singleChat = chats.find((chat) => chat._id === req.params.id);
+  res.send(singleChat);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server started on PORT ${PORT}`));

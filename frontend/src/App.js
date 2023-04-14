fc1f26479c10fef820b8dc1b45d5b2bc035e83b5
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-04-14 14:02:15 +0530

    Home route & chats route added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import ChatPage from "./pages/ChatPage";
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="chats" element={<ChatPage />} />
+    </Routes>
   );
 }
 

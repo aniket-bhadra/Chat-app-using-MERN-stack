87c9d30d2687bde3e8442ff406f5e7d88a38509a
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-04-19 20:24:15 +0530

    user authentication routes added

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const colors = require("colors");
 
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
 
 dotenv.config();
 
@@ -14,14 +15,7 @@ app.get("/", (req, res) => {
   res.send("Api is running successfully");
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
-
-app.get("/api/chat/:id", (req, res) => {
-  const singleChat = chats.find((chat) => chat._id === req.params.id);
-  res.send(singleChat);
-});
+app.use("/api/user", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server started on PORT ${PORT}`.yellow.bold));

02b5bef7b084cca12236e372ddae3dca66f03ff5
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-04-20 11:29:27 +0530

    login functionality added

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -39,6 +39,27 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  // console.log(user)
+
+  if (user && (await user.matchPassword(password))) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid Email or Password");
+  }
+});
+
 module.exports = {
   registerUser,
+  authUser,
 };

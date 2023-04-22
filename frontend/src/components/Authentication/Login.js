fecbf2b207a93c4cdfe347f1b0241acae401fa46
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-04-22 14:04:43 +0530

    login functionality added

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -7,17 +7,77 @@ import {
   InputGroup,
   InputRightElement,
   VStack,
+  useToast,
 } from "@chakra-ui/react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [show, setShow] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const toast = useToast();
 
   const passwordHiddingHandler = () =>
     setShow((prevShowValue) => !prevShowValue);
 
-  const onSubmitHandler = () => {};
+  const onSubmitHandler = async () => {
+    setLoading(true);
+    if (!email || !password) {
+      toast({
+        title: "Please Fill all the Fields",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+
+      const { data } = await axios.post(
+        "/api/user/login",
+        {
+          email,
+          password,
+        },
+        config
+      );
+
+      toast({
+        title: "Login Successful",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      setLoading(false);
+      navigate("chats", {
+        replace: true,
+      });
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: error.response.data.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+    }
+  };
 
   return (
     <VStack spacing="5px">
@@ -51,8 +111,9 @@ const Login = () => {
         width="100%"
         style={{ marginTop: 15 }}
         onClick={onSubmitHandler}
+        isLoading={loading}
       >
-       Login
+        Login
       </Button>
       <Button
         variant="solid"
@@ -63,7 +124,7 @@ const Login = () => {
           setPassword("123456");
         }}
       >
-       Explore as a Guest
+        Explore as a Guest
       </Button>
     </VStack>
   );

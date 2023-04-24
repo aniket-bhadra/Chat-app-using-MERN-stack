aceedd6f6ccdcd9d0e8226d6245c6da02391f898
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-04-24 23:10:37 +0530

    all chat routes added

---
+++ backend/routes/chatRoutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const { protect } = require("../middleware/authMiddleware");
+
+const router = express.Router();
+
+//creating chat for that user
+router.route("/").post(protect, accessChat);
+// //fetching a chats for that user
+// router.route("/").get(protect, fetchChats);
+// //creation of the group
+// router.route("/group").post(protect, createGroupChat);
+// //rename a particaular group
+// router.route("/rename").put(protect, renameGroup);
+// //remove someone from group or leave a group
+// router.route("/groupremove").put(protect, removeFromGroup);
+// //add someone to the group
+// router.route("/groupadd").put(protect, addToGroup);
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const colors = require("colors");
 
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
+const chatRoutes = require("./routes/chatRoutes");
 const userRoutes = require("./routes/userRoutes");
 const {
   notFoundMiddleware,
@@ -22,6 +23,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);

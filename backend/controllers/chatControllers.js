d7614fe4c215c428176c4a62be4c69d8d99c923f
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-04-27 23:58:21 +0530

    fixing certain things in accessControllers

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -2,11 +2,23 @@ const asyncHandler = require("express-async-handler");
 const Chat = require("../models/chatModel");
 
 const accessChat = asyncHandler(async (req, res) => {
-  //responsible for creating & fetching one and one chat
+  //responsible for creating & fetching one and one chat, not group chat
   const { userId } = req.body;
 
   if (!userId) {
+    // ! this code needs to revisit--console.log() part
     console.log("UserId param not sent with request");
     return res.sendStatus(400);
   }
+
+  //now checks whether the chat exist with that user with login user
+  let isChat = await Chat.find({
+    isGroupChat: false,
+
+    //in case of $and, both of the condition has to match to return any document
+    $and: [
+      { users: { $elemMatch: { $eq: req.user._id } } },
+      { users: { $elemMatch: { $eq: userId } } },
+    ],
+  });
 });

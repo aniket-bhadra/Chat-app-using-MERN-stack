7b746f77c8e6d8f3559c0136905c31ba33bbab13
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-05-09 14:00:28 +0530

    folder structure updated for MyChats & ChatBox components

--- frontend/src/pages/ChatPage.js
+++ frontend/src/pages/ChatPage.js
@@ -2,8 +2,8 @@ import { Box } from "@chakra-ui/react";
 
 import { useChatState } from "../Context/ChatProvider";
 import SideDrawer from "../components/miscellaneous/SideDrawer";
-import MyChats from "../components/miscellaneous/MyChats";
-import ChatBox from "../components/miscellaneous/ChatBox";
+import MyChats from "../components/MyChats";
+import ChatBox from "../components/ChatBox";
 
 const ChatPage = () => {
   const { user } = useChatState();

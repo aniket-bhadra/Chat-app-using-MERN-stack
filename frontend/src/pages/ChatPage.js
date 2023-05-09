5532738dc311141e84380236a41394c10974c004
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-05-09 15:10:18 +0530

    few style issues fixed

--- frontend/src/pages/ChatPage.js
+++ frontend/src/pages/ChatPage.js
@@ -11,8 +11,13 @@ const ChatPage = () => {
   return (
     <div style={{ width: "100%" }}>
       {user && <SideDrawer />}
-
-      <Box>
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        width="100%"
+        height="91.5vh"
+        padding="10px"
+      >
         {user && <MyChats />}
         {user && <ChatBox />}
       </Box>

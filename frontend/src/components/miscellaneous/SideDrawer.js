caedea74568decabcce7c04906a72e1ab755d6c8
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-05-10 13:17:34 +0530

    top section completed

--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -1,9 +1,78 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
+  Text,
+  Tooltip,
+} from "@chakra-ui/react";
+import { BellIcon, ChevronDownIcon } from "@chakra-ui/icons";
+import { useChatState } from "../../Context/ChatProvider";
 
 const SideDrawer = () => {
-  return (
-    <div>SideDrawer</div>
-  )
-}
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [loadingChat, setLoadingChat] = useState();
 
-export default SideDrawer
+  const { user } = useChatState();
+  return (
+    <>
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        bg="white"
+        width="100%"
+        padding="5px 10px 5px 10px"
+        borderWidth="5px"
+      >
+        <Tooltip label="Search Users to chat" hasArrow placement="bottom-end">
+          <Button variant="ghost">
+            <i class="fas fa-search"></i>
+            <Text display={{ base: "none", md: "flex" }} px="4">
+              Search User
+            </Text>
+          </Button>
+        </Tooltip>
+
+        <Text fontSize="2xl" fontFamily="Work sans">
+          Talk2Owl
+        </Text>
+        <div>
+          <Menu>
+            <MenuButton padding={1}>
+              <BellIcon fontSize="2xl" m={1} />
+            </MenuButton>
+
+            {/* <MenuList></MenuList> */}
+          </Menu>
+
+          <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+              <Avatar
+                size="sm"
+                cursor="pointer"
+                name={user.name}
+                src={user.pic}
+              />
+            </MenuButton>
+
+            <MenuList>
+              <MenuItem>My Profile</MenuItem>
+              <MenuDivider />
+              <MenuItem>Logout</MenuItem>
+            </MenuList>
+          </Menu>
+        </div>
+      </Box>
+    </>
+  );
+};
+
+export default SideDrawer;

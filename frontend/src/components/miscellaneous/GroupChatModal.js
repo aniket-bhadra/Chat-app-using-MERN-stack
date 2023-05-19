e1a85e8a6686c1ad249796d86546202645eba4eb
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-05-19 22:40:43 +0530

    modal created

---
+++ frontend/src/components/miscellaneous/GroupChatModal.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  ModalFooter,
+  useDisclosure,
+  useToast,
+} from "@chakra-ui/react";
+
+import { useChatState } from "../../Context/ChatProvider";
+
+const GroupChatModal = ({ children }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [groupChatName, setGroupChatName] = useState();
+  const [selectedUsers, setSelectedUsers] = useState([]);
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setloading] = useState(false);
+  const toast = useToast();
+  const { user, chats, setChats } = useChatState();
+
+  return (
+    <>
+      <span onClick={onOpen}>{children}</span>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody></ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default GroupChatModal;

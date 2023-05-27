6fa18ecd5fba4acab4bb4b47fe3631a9a51926d5
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-05-27 22:47:03 +0530

    ChatBox created

--- frontend/src/components/ChatBox.js
+++ frontend/src/components/ChatBox.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Box } from "@chakra-ui/react";
 
-const ChatBox = () => {
+import { useChatState } from "../Context/ChatProvider";
+
+const ChatBox = ({ fetchAgain, setFetchAgain }) => {
+  const { selectedChat } = useChatState();
+
   return (
-    <div>ChatBox</div>
-  )
-}
+    <Box
+      display={{ base: selectedChat ? "flex" : "none", md: "flex" }}
+      alignItems="center"
+      flexDir="column"
+      p={3}
+      bg="white"
+      w={{ base: "100%", md: "68%" }}
+      borderRadius="lg"
+      borderWidth="1px"
+    >
+      <SingleChat fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+    </Box>
+  );
+};
 
-export default ChatBox
+export default ChatBox;

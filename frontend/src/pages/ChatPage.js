52c40c12ffb20a59d6908deefe46e23ce4b2cef8
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-05-29 14:57:19 +0530

    new states added & passed to the childs

--- frontend/src/pages/ChatPage.js
+++ frontend/src/pages/ChatPage.js
@@ -4,9 +4,11 @@ import { useChatState } from "../Context/ChatProvider";
 import SideDrawer from "../components/miscellaneous/SideDrawer";
 import MyChats from "../components/MyChats";
 import ChatBox from "../components/ChatBox";
+import { useState } from "react";
 
 const ChatPage = () => {
   const { user } = useChatState();
+  const [fetchAgain, setFetchAgain] = useState(false);
 
   return (
     <div style={{ width: "100%" }}>
@@ -18,8 +20,10 @@ const ChatPage = () => {
         height="91.5vh"
         padding="10px"
       >
-        {user && <MyChats />}
-        {user && <ChatBox />}
+        {user && <MyChats fetchAgain={fetchAgain} />}
+        {user && (
+          <ChatBox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+        )}
       </Box>
     </div>
   );

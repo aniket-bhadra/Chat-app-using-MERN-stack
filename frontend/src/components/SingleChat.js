cb58bab20eeb0c2cb29c9af1f8927bcfc33503df
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-05-31 11:10:37 +0530

    the structure of ChatBox created

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -1,8 +1,11 @@
 import React from "react";
-
-import { useChatState } from "../Context/ChatProvider";
 import { Box, IconButton, Text } from "@chakra-ui/react";
 import { ArrowBackIcon } from "@chakra-ui/icons";
+
+import { useChatState } from "../Context/ChatProvider";
+import { getSender, getSenderFull } from "../config/ChatLogics";
+import ProfileModal from "./miscellaneous/ProfileModal";
+import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
   const { user, selectedChat, setSelectedChat } = useChatState();
@@ -25,7 +28,38 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
               icon={<ArrowBackIcon />}
               onClick={() => setSelectedChat("")}
             />
+
+            {!selectedChat.isGroupChat ? (
+              <>
+                {getSender(user, selectedChat.users)}
+                <ProfileModal user={getSenderFull(user, selectedChat.users)} />
+                
+              </>
+            ) : (
+              <>
+                {selectedChat.chatName.toUpperCase()}
+                <UpdateGroupChatModal
+                  fetchAgain={fetchAgain}
+                  setFetchAgain={setFetchAgain}
+                />
+              </>
+            )}
           </Text>
+
+          <Box
+          display="flex"
+          flexDir="column"
+          justifyContent="flex-end"
+          p={3}
+          bg="#E8E8E8"
+          w="100%"
+          h="100%"
+          borderRadius="lg"
+          overflowY="hidden"
+          > 
+
+            {/* Messages here */}
+          </Box>
         </>
       ) : (
         <Box

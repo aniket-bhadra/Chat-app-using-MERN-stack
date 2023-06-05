1b8d667f98f852fc2a4ac6206a10314d4ad4652b
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-06-05 23:06:36 +0530

    sendMessage controller created

---
+++ backend/controllers/messageControllers.js
@@ -0,0 +1,55 @@
+const asyncHandler = require("express-async-handler");
+
+const Message = require("../models/messageModel");
+const User = require("../models/userModel");
+const Chat = require("../models/chatModel");
+
+const sendMessages = asyncHandler(async (req, res) => {
+  const { content, chatId } = req.body;
+
+  if (!content || !chatId) {
+    // ! instead of console.log here, use ------ new Error("")
+    console.log("Invalid data passed into request");
+    return res.sendStatus(400);
+  }
+
+  // * here we need to add one extra layer of validation where we will check chatId's users array, before sending message to chat,  need to check bearer token's userId inside that given chatId's users array, it help us-- that only the users part of that chat, can send message inside that chat.
+
+  let newMessage = {
+    sender: req.user._id,
+    content: content,
+    chat: chatId,
+  };
+
+  try {
+    let message = await Message.create(newMessage);
+
+    //since here we are not populating directly inside of a query, we are populating an instance that is why here---used execPopulate()---->(deprecated)
+
+    message = await message.populate("sender", "name pic");
+    message = await message.populate("chat");
+    message = await User.populate(message, {
+      path: "chat.users",
+      select: "name pic email",
+    });
+    console.log(message);
+
+    const yo = await Chat.findByIdAndUpdate(
+      req.body.chatId,
+      {
+        latestMessage: message,
+      },
+      {
+        new: true,
+      }
+    );
+    console.log("yo is ", yo);
+
+    res.json(message);
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});
+
+module.exports = { sendMessages };
--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const { protect } = require("../middleware/authMiddleware");
+const { sendMessages } = require("../controllers/messageControllers");
 
 router.route("/").post(protect, sendMessages);
 // router.route("/:chatId").get(protect, allMessages);

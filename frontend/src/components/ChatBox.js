f5bc64dbf3e65efd571ec9e4a42adec5627770d5
aniket-bhadra <aniketbhadra98320@gmail.com>
2025-03-06 16:18:03 +0530

    chatbox styles updated

--- frontend/src/components/ChatBox.js
+++ frontend/src/components/ChatBox.js
@@ -3,6 +3,7 @@ import { Box } from "@chakra-ui/react";
 
 import { useChatState } from "../Context/ChatProvider";
 import SingleChat from "./SingleChat";
+import theme from "../constants/theme";
 
 const ChatBox = ({ fetchAgain, setFetchAgain }) => {
   const { selectedChat } = useChatState();
@@ -12,11 +13,16 @@ const ChatBox = ({ fetchAgain, setFetchAgain }) => {
       display={{ base: selectedChat ? "flex" : "none", md: "flex" }}
       alignItems="center"
       flexDir="column"
-      p={3}
+      p={4}
       bg="white"
       w={{ base: "100%", md: "68%" }}
       borderRadius="lg"
       borderWidth="1px"
+      borderColor={theme.light}
+      boxShadow="0 2px 10px rgba(0,0,0,0.05)"
+      height="100%"
+      position="relative"
+      overflow="hidden"
     >
       <SingleChat fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
     </Box>
